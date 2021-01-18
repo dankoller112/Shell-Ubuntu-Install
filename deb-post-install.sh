853776beb6927cf61702569ed87a1ea5c0429c6b
Dan Koller <57103678+dankoller112@users.noreply.github.com>
2021-01-18 11:51:24 +0100

    Update deb-post-install.sh

--- deb-post-install.sh
+++ deb-post-install.sh
@@ -6,4 +6,7 @@ apt install screenfetch -y
 apt install gnome-tweaks -y
 apt install network-manager-openvpn-gnome openconnect network-manager-openconnect network-manager-openconnect-gnome -y
 apt install texlive texlive-lang-german texlive-latex-extra -y
+snap install chromium chromium-ffmpeg
+snap install --classic eclipse
+snap install --classic code
 #etc
