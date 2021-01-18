49ce54efd00d579d0c8bba182337c301071745d0
Dan Koller <57103678+dankoller112@users.noreply.github.com>
2021-01-18 07:58:39 +0100

    Inititial commit
    Other programs following soon

---
+++ deb-post-install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+apt update
+apt upgrade -y
+apt autoremove -y
+apt install screenfetch -y
+snap install chromium chromium-ffmpeg -y
+apt install gnome-tweaks -y
+apt install network-manager-openvpn-gnome openconnect network-manager-openconnect network-manager-openconnect-gnome -y
+apt install texlive texlive-lang-german texlive-latex-extra -y
+snap install --classic eclipse -y
+#etc
