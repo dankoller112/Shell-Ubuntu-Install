8fef57431d8b33802435967fe54aab376a6a6790
Dan Koller <57103678+dankoller112@users.noreply.github.com>
2021-01-18 13:45:13 +0100

    Add files via upload

--- deb-post-install.sh
+++ deb-post-install.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
-sudo apt update
-sudo apt upgrade -y
-sudo apt autoremove -y
-sudo apt install screenfetch -y
-sudo apt install gnome-tweaks -y
-sudo apt install network-manager-openvpn-gnome openconnect network-manager-openconnect network-manager-openconnect-gnome -y
-sudo apt install texlive texlive-lang-german texlive-latex-extra -y
-sudo snap install chromium chromium-ffmpeg
-sudo snap install --classic eclipse
-sudo snap install --classic code
+#!/bin/sh
+apt update
+apt upgrade -y
+apt autoremove -y
+apt install screenfetch -y
+apt install gnome-tweaks -y
+apt install network-manager-openvpn-gnome openconnect network-manager-openconnect network-manager-openconnect-gnome -y
+apt install texlive texlive-lang-german texlive-latex-extra -y
+snap install chromium chromium-ffmpeg
+snap install --classic eclipse
+snap install --classic code
 #etc
