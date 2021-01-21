81683266363b70a59a4b7ff1331259f51bdcfcc2
Dan Koller <57103678+dankoller112@users.noreply.github.com>
2021-01-21 21:11:49 +0100

    Update deb-post-install.sh
    + Added OpenJDK 11 installation

--- deb-post-install.sh
+++ deb-post-install.sh
@@ -4,6 +4,7 @@ sudo apt upgrade -y
 sudo apt autoremove -y
 sudo apt install screenfetch -y
 sudo apt install gnome-tweaks -y
+sudo apt-get install openjdk-11-jdk -y
 sudo apt install network-manager-openvpn-gnome openconnect network-manager-openconnect network-manager-openconnect-gnome -y
 sudo apt install texlive texlive-lang-german texlive-latex-extra -y
 sudo apt install texmaker -y
