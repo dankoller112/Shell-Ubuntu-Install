29143ef5322dd4ada9ba3225457f3558779f790f
Dan Koller <57103678+dankoller112@users.noreply.github.com>
2021-01-25 07:25:12 +0100

    Update deb-post-install.sh
    + Added "Sublime Text 3" installation commands

--- deb-post-install.sh
+++ deb-post-install.sh
@@ -8,6 +8,9 @@ sudo apt install openjdk-11-jdk openjdk-11-doc openjdk-11-source -y
 sudo apt install build-essential -y
 sudo apt install python3 -y
 sudo apt install filezilla -y
+sudo apt install apt-transport-https -y
+sudo wget -q -O "-" https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt install sublime-text -y
 sudo apt install network-manager-openvpn-gnome openconnect network-manager-openconnect network-manager-openconnect-gnome -y
 sudo apt install texlive texlive-lang-german texlive-latex-extra -y
 sudo apt install texmaker -y
