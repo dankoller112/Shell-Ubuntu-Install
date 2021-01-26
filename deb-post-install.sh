f488f4f6f23725c24c812f82ba668732b70db88f
Dan Koller <57103678+dankoller112@users.noreply.github.com>
2021-01-26 07:18:51 +0100

    Update deb-post-install.sh
    + Added "sublime merge" installation
    + Rearranged installation order

--- deb-post-install.sh
+++ deb-post-install.sh
@@ -8,15 +8,16 @@ sudo apt install openjdk-11-jdk openjdk-11-doc openjdk-11-source -y
 sudo apt install build-essential -y
 sudo apt install python3 -y
 sudo apt install filezilla -y
-sudo apt install apt-transport-https -y
-sudo wget -q -O "-" https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt install sublime-text -y
 sudo apt install network-manager-openvpn-gnome openconnect network-manager-openconnect network-manager-openconnect-gnome -y
 sudo apt install texlive texlive-lang-german texlive-latex-extra -y
 sudo apt install texmaker -y
 sudo apt install git -y
 sudo apt install zsh -y
 sudo sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo apt install apt-transport-https -y
+sudo wget -q -O "-" https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt install sublime-text -y
+sudo apt install sublime-merge -y
 sudo snap install chromium chromium-ffmpeg
 sudo snap install --classic eclipse
 sudo snap install --classic code
