dc4f7fee1362043bc88f0ae423390cab7a4cfdff
Dan Koller <57103678+dankoller112@users.noreply.github.com>
2021-01-27 20:25:25 +0100

    Update deb-post-install.sh
    + Added Sublime Text & Merge repositories to download (stable channel)

--- deb-post-install.sh
+++ deb-post-install.sh
@@ -18,6 +18,8 @@ sudo apt install zsh -y
 sudo sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sudo wget -q -O "-" https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt install apt-transport-https -y
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt install sublime-text -y
 sudo apt install sublime-merge -y
 sudo snap install chromium chromium-ffmpeg
