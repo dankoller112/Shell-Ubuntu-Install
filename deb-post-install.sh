ffa81f250d6ae97de0a269551795bbaa6c32c59e
Dan Koller <57103678+dankoller112@users.noreply.github.com>
2021-01-27 20:39:44 +0100

    Update deb-post-install.sh
    + Reconfigured sublime software installation

--- deb-post-install.sh
+++ deb-post-install.sh
@@ -4,6 +4,7 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt autoremove -y
+# Standard packages
 sudo apt install screenfetch -y
 sudo apt install gnome-tweaks -y
 sudo apt install openjdk-11-jdk openjdk-11-doc openjdk-11-source -y
@@ -16,8 +17,14 @@ sudo apt install texmaker -y
 sudo apt install git -y
 sudo apt install zsh -y
 sudo sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sudo snap install --classic sublime-text
-sudo snap install --classic sublime-merge
+# Sublime (Text & Merge) packages
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update
+sudo apt install sublime-text -y
+sudo apt install sublime-merge -y
+# Snap packages
 sudo snap install chromium chromium-ffmpeg
 sudo snap install --classic eclipse
 sudo snap install --classic code
