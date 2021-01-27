d5e36f3bfae2690aade2a45b21676aa2ac5fa94d
Dan Koller <57103678+dankoller112@users.noreply.github.com>
2021-01-27 20:27:56 +0100

    Update deb-post-install.sh
    + Replaced sublime text & merge with "snap" package manager commands

--- deb-post-install.sh
+++ deb-post-install.sh
@@ -16,12 +16,8 @@ sudo apt install texmaker -y
 sudo apt install git -y
 sudo apt install zsh -y
 sudo sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sudo wget -q -O "-" https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt install apt-transport-https -y
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt install sublime-text -y
-sudo apt install sublime-merge -y
+sudo snap install --classic sublime-text
+sudo snap install --classic sublime-merge
 sudo snap install chromium chromium-ffmpeg
 sudo snap install --classic eclipse
 sudo snap install --classic code
