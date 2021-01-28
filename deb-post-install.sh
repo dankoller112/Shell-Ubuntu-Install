86ea670d24a69ae5b5a5ef04c23b3d6c78fbe79c
Dan Koller <57103678+dankoller112@users.noreply.github.com>
2021-01-28 07:11:20 +0100

    Update deb-post-install.sh
    - Changed description comment

--- deb-post-install.sh
+++ deb-post-install.sh
@@ -17,7 +17,7 @@ sudo apt install texmaker -y
 sudo apt install git -y
 sudo apt install zsh -y
 sudo sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# Sublime (Text & Merge) packages
+# Sublime packages
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt-get install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
