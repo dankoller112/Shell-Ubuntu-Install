6fab010dc64dc227b07c6eb763ebea866a1b5c5f
Dan Koller <57103678+dankoller112@users.noreply.github.com>
2021-01-30 12:20:42 +0100

    Update deb-post-install.sh
    + Added break due to a bug creating an endless loop

--- deb-post-install.sh
+++ deb-post-install.sh
@@ -11,7 +11,7 @@ while true; do
     case $yn in
         [Yy]* ) sudo apt install snap -y && apt install snapd -y; break;;
         [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
+        * ) echo "Please answer yes or no."; break;
     esac
 done
 
