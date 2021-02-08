f5da3ac8a509b908b1fda354c50e5bbe17a0a177
Dan Koller <57103678+dankoller112@users.noreply.github.com>
2021-02-08 15:08:55 +0100

    + Rearranged installation order Standard packages and dev tools now split

--- deb-post-install.sh
+++ deb-post-install.sh
@@ -37,13 +37,17 @@ sudo apt install firefox language-pack-de-base -y
 sudo apt install thunderbird -y
 sudo apt install screenfetch -y
 sudo apt install gnome-tweaks -y
+sudo apt install network-manager-openvpn-gnome openconnect network-manager-openconnect network-manager-openconnect-gnome -y
+sudo apt install texlive texlive-lang-german texlive-latex-extra -y
+sudo apt install texmaker -y
+
+echo "Done"
+echo "Installing developer tools"
+
 sudo apt install openjdk-11-jdk openjdk-11-doc openjdk-11-source -y
 sudo apt install build-essential -y
 sudo apt install python3 -y
 sudo apt install filezilla -y
-sudo apt install network-manager-openvpn-gnome openconnect network-manager-openconnect network-manager-openconnect-gnome -y
-sudo apt install texlive texlive-lang-german texlive-latex-extra -y
-sudo apt install texmaker -y
 sudo apt install git -y
 sudo apt install zsh -y
 sudo sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
